b4683cfc48c87619b17db4f958a848baf95fad9b
Dakotah Ducharme <dakotahbducharme@gmail.com>
2019-03-18 13:47:39 -0700

    Added header info and bio

---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const MyHeader = styled.nav`
+${'' /* background-color: #f2b093; */}
+  flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+  #container {
+    display: inline-flex;
+  }
+  li {
+    display: inline-flex;
+    text-decoration: none;
+    padding: 10px;
+  }
+  a {
+    text-decoration: none;
+    color: #1e1e1e;
+  }
+  a:hover {
+    color: grey;
+  }
+  h1 {
+    padding: 0 20px;
+    color: #f2b093;
+  }
+`
+class Header extends Component {
+  render() {
+    return (
+      <MyHeader>
+        <div id="container">
+          <h1>dakotah ducharme</h1>
+          <ul>
+            <li><a href="https://www.github.com/dakotahducharme">github</a></li>
+            <li><a href="https://www.twitter.com/devdbd">twitter</a></li>
+            <li><a href="">contact me</a></li>
+            <li><a href=""></a></li>
+          </ul>
+        </div>
+      </MyHeader>
+    )
+  }
+}
+
+export default Header;
--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+
+class Home extends Component {
+  render(){
+    return(
+      <div>
+        <p>I am a Full Stack Developer living in the Bay Area, with a passion for all things tech. In addition to development, I am also an artist in my spare time, and live for the feeling of empowerment after I create something meaningful. While I am young, I am extremely motivated, and believe that stepping out of your comfort zone is crucial for personal growth. I am a true empath who seeks to better the world, through a screen or otherwise. I am fascinated by innovative technologies and applications, and the way they function.<br/>
+        My favorite technologies to work with: React, Ruby, HTML, CSS, JS<br/>
+
+        I am also highly proficient in JQuery, express, node, and mongoose</p>
+      </div>
+    )
+  }
+}
+
+export default Home;
